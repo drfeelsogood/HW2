f12bcb76e7f4a2d2bdd24888f91ef5a7e5eba7b3
Eren Evirgen <Eren Evirgen>
2016-06-14 22:44:52 +0300

    'ca2'

---
+++ ca2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            Random rnd = new Random();
+            int number;
+            
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                number = rnd.Next(20);
+                if (Array.IndexOf(myArray, number) == -1)
+                {
+                    myArray[i] = number;
+                    Console.WriteLine(myArray[i]);
+                }
+                else
+                {
+                    i--;
+                }                
+            }
+            number = myArray.Max();
+            Console.WriteLine("Biggest number is: " + number.ToString());
+            Console.ReadKey();
+        }
+    }
+}
